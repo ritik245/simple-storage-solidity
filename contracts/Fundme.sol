91f6cf2998d21bfc6d63624e58ff5706ece63e04
ritik245 <evilgenious15@gmail.com>
2026-01-29 00:20:25 +0530

    added withdraw function

--- contracts/Fundme.sol
+++ contracts/Fundme.sol
@@ -11,6 +11,12 @@ contract FundMe {
    uint256 public minimumUsd=50*1e18;
    address[] public funders;
    mapping(address => uint256) public addressToAmountFunded;
+   address public owner;
+
+
+   constructor(){
+      owner =msg.sender;
+   }
 
     function fund() public payable{
         // want  to be able to set a min fund
@@ -19,6 +25,32 @@ contract FundMe {
         //reverting undo any action before,and send reamining gas back
         funders.push(msg.sender);
         addressToAmountFunded[msg.sender]=msg.value;
+    }
+
+    function withdraw() public onlyOwner {
+        //require(msg.sender== owner,"Sender is not owner");
+        //for loop
+        for(uint256 funderIndex=0;funderIndex < funders.length;funderIndex++){
+            address funder=funders[funderIndex];
+            addressToAmountFunded[funder]=0;
+        }
+        //reset array
+        funders=new address[](0);
+        // actually withdraw
+        //transfer
+        payable(msg.sender).transfer(address(this).balance);//if failed it will throw error
+        //send
+        bool sendSuccess=payable(msg.sender).send(address(this).balance);
+        require(sendSuccess,"Send failed");//if failed will send bool message
+        //call
+        (bool callSuccess,)=payable(msg.sender).call{value:address(this).balance}("");
+        require(callSuccess,"Call failed");
+
+    }
+
+    modifier onlyOwner  {
+        require(msg.sender==owner,"Sender is not owner!");
+        _;
     }
 
     
